b0a4c8f18c6f9768fdac00c4ca7a02e2eee9798a
Electro-Corp <chinmaytanmay@outlook.com>
2025-11-28 11:39:32 -0700

    make color calib show up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/ColorSensorCalib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/ColorSensorCalib.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.opsmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
+@TeleOp(name="ColorCalib")
 public class ColorSensorCalib extends LinearOpMode {
     private NormalizedColorSensor leftColor;
     private NormalizedColorSensor midColor;
