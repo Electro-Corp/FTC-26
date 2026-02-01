a526fddc919cfe8de1301df819472f0ff791f93a
Electro-Corp <chinmaytanmay@outlook.com>
2026-01-31 19:10:56 -0700

    FTC Idaho State 2026 changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -232,7 +232,8 @@ public class Shooter implements Runnable{
                 break;
         }
 
-        if(state == ShooterState.STOPPED || state == ShooterState.REVERSE) setDamUp();
+        if(state == ShooterState.STOPPED || state == ShooterState.REVERSE || state == ShooterState.WAITING_FOR_SPIN_UP) setDamUp();
+        else if (state == ShooterState.SPIN_UP_HOLD && !readColorsOnce) setDamUp();
         else setDamDown();
     }
 
