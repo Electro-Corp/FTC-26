e9248a48103d8ae1dd105b2cfc87d629ee47abe6
sammypbaird <sammypbaird@gmail.com>
2026-02-21 23:06:49 -0700

    Adding pedro path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/RoadRunnerTest.java
@@ -1,43 +1,43 @@
-package org.firstinspires.ftc.teamcode.opsmodes;
-
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
-import org.firstinspires.ftc.teamcode.roadrunner.PinpointLocalizer;
-
-@TeleOp(name="RoadRunner Test", group="TeleOp")
-public class RoadRunnerTest extends LinearOpMode {
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        Pose2d initPose = new Pose2d(0,0,0);
-        MecanumDrive drive = new MecanumDrive(hardwareMap, initPose);
-
-        MecanumDrive.PARAMS.maxWheelVel = 20;
-        MecanumDrive.PARAMS.lateralGain = 0;
-        MecanumDrive.PARAMS.headingGain = 5;
-
-        PinpointLocalizer.PARAMS.parYTicks
-
-        TrajectoryActionBuilder trajectory = drive.actionBuilder(initPose)
-                        .lineToX(30)
-                        .turn(Math.PI / 2)
-                        .lineToY(30);
-
-        waitForStart();
-
-        Action trajAction = trajectory.build();
-
-        Actions.runBlocking(trajAction);
-
-        if(isStopRequested()) return;
-
-
-    }
-}
+//package org.firstinspires.ftc.teamcode.opsmodes;
+//
+//import com.acmerobotics.roadrunner.Action;
+//import com.acmerobotics.roadrunner.Pose2d;
+//import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+//import com.acmerobotics.roadrunner.Vector2d;
+//import com.acmerobotics.roadrunner.ftc.Actions;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+//import org.firstinspires.ftc.teamcode.roadrunner.PinpointLocalizer;
+//
+//@TeleOp(name="RoadRunner Test", group="TeleOp")
+//public class RoadRunnerTest extends LinearOpMode {
+//
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//        Pose2d initPose = new Pose2d(0,0,0);
+//        MecanumDrive drive = new MecanumDrive(hardwareMap, initPose);
+//
+//        MecanumDrive.PARAMS.maxWheelVel = 20;
+//        MecanumDrive.PARAMS.lateralGain = 0;
+//        MecanumDrive.PARAMS.headingGain = 5;
+//
+//        PinpointLocalizer.PARAMS.parYTicks
+//
+//        TrajectoryActionBuilder trajectory = drive.actionBuilder(initPose)
+//                        .lineToX(30)
+//                        .turn(Math.PI / 2)
+//                        .lineToY(30);
+//
+//        waitForStart();
+//
+//        Action trajAction = trajectory.build();
+//
+//        Actions.runBlocking(trajAction);
+//
+//        if(isStopRequested()) return;
+//
+//
+//    }
+//}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+// See Web Browser Panels here: http://192.168.43.1:8001/
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants().mass(5);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(3.7)
+            .strafePodX(0)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightBack")
+            .leftRearMotorName("leftBack")
+            .leftFrontMotorName("leftFront")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .build();
+    }
+}
