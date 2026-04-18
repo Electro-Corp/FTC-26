ceb961271156e20a11341caa71f4f27c338cbf65
sammypbaird <sammypbaird@gmail.com>
2026-04-18 17:02:25 -0600

    Rotate the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroTestSarah.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroTestSarah.java
@@ -18,6 +18,7 @@ public class PedroTestSarah extends OpMode {
         //START POSITION_END POSITION
         DRIVE_STARTPOS_SHOOT_POS,
         LOOK_AT_APRILTAG,
+        TURN_TO_SHOOT,
         SHOOT_PRELOAD,
         GRAB_MORE_ARTIFACTS_ONE
     }
@@ -26,21 +27,17 @@ public class PedroTestSarah extends OpMode {
 
     private final Pose startPose = new Pose(123.9052132701422, 123.50710900473929, Math.toRadians(36));
     private final Pose aprilTagPose = new Pose(87.651, 87.974, Math.toRadians(95));
-    private final Pose shootPose = new Pose(99.65876777251184, 98.82464454976305, Math.toRadians(36)); //TODO how to just turn without moving
+    private final Pose shootPose = new Pose(87.651, 87.974, Math.toRadians(45));
     private final Pose turnToGrabOne = new Pose(94.521327014218, 83.75355450236967, Math.toRadians(0));
     private final Pose grabOneArtifact = new Pose(129.5734597156398, 84.12322274881517, Math.toRadians(0));
 
-    private PathChain driveStartPosShootPos, driveAprilTagShootPos, driveGrabArtifactOne;
+    private PathChain driveStartPosShootPos, driveGrabArtifactOne;
 
     public void buildPaths(){
         //put in coordinates for starting pose > ending pose
         driveStartPosShootPos = follower.pathBuilder()
                 .addPath(new BezierLine(startPose, aprilTagPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), aprilTagPose.getHeading())
-                .build();
-        driveAprilTagShootPos = follower.pathBuilder()
-                .addPath(new BezierLine(aprilTagPose, shootPose))
-                .setLinearHeadingInterpolation(aprilTagPose.getHeading(), shootPose.getHeading())
                 .build();
         driveGrabArtifactOne = follower.pathBuilder()
                 .addPath(new BezierLine(shootPose, turnToGrabOne))
@@ -56,21 +53,23 @@ public class PedroTestSarah extends OpMode {
                 setPathState(PathState.LOOK_AT_APRILTAG); //reset timer and make new state
                 break;
             case LOOK_AT_APRILTAG:
-                //check is follower done it's path?
-                //check that 3 seconds has elapsed
                 if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() > 3) {
                     telemetry.addLine("Done Path 1");
-                    follower.followPath(driveAprilTagShootPos, true);
+                    follower.turnTo(shootPose.getHeading());
+                    setPathState(PathState.TURN_TO_SHOOT);
+                }
+                break;
+            case TURN_TO_SHOOT:
+                if (!follower.isTurning()) {
+                    telemetry.addLine("Done turning to shoot heading");
                     setPathState(PathState.SHOOT_PRELOAD);
-                    //transition to next state
                 }
                 break;
             case SHOOT_PRELOAD:
-                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() > 1) {
+                if (pathTimer.getElapsedTimeSeconds() > 1) {
                     telemetry.addLine("done with Pose 2");
                     follower.followPath(driveGrabArtifactOne, true);
                     setPathState(PathState.GRAB_MORE_ARTIFACTS_ONE);
-                    //transition to next state
                 }
                 break;
             case GRAB_MORE_ARTIFACTS_ONE:
