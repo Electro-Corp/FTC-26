2cae6a045165e271d240fd4d2d6441b9e65c49f0
sammypbaird <sammypbaird@gmail.com>
2026-04-18 12:30:05 -0600

    Adding more to Sarah's path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroTestSarah.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroTestSarah.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.pedropathing.util.Timer;
 
-@Autonomous
+@Autonomous (name="Most Amazingest Auto Everrrrrrr", group = "Autonomous")
 public class PedroTestSarah extends OpMode {
 
     private Follower follower;
@@ -17,38 +17,66 @@ public class PedroTestSarah extends OpMode {
     public enum PathState {
         //START POSITION_END POSITION
         DRIVE_STARTPOS_SHOOT_POS,
-        SHOOT_PRELOAD
+        LOOK_AT_APRILTAG,
+        SHOOT_PRELOAD,
+        GRAB_MORE_ARTIFACTS_ONE
     }
 
     PathState pathState;
 
-    private final Pose startPose = new Pose(123.9052132701422, 123.50710900473929, Math.toRadians(45));
-    private final Pose endPose = new Pose(87.13879003558719, 89.85053380782918, Math.toRadians(45));
+    private final Pose startPose = new Pose(123.9052132701422, 123.50710900473929, Math.toRadians(36));
+    private final Pose aprilTagPose = new Pose(87.651, 87.974, Math.toRadians(95));
+    private final Pose shootPose = new Pose(99.65876777251184, 98.82464454976305, Math.toRadians(36)); //TODO how to just turn without moving
+    private final Pose turnToGrabOne = new Pose(94.521327014218, 83.75355450236967, Math.toRadians(0));
+    private final Pose grabOneArtifact = new Pose(129.5734597156398, 84.12322274881517, Math.toRadians(0));
 
-    private PathChain driveStartPosShootPos;
+    private PathChain driveStartPosShootPos, driveAprilTagShootPos, driveGrabArtifactOne;
 
     public void buildPaths(){
         //put in coordinates for starting pose > ending pose
         driveStartPosShootPos = follower.pathBuilder()
-                .addPath(new BezierLine(startPose, endPose))
-                .setLinearHeadingInterpolation(startPose.getHeading(), endPose.getHeading())
+                .addPath(new BezierLine(startPose, aprilTagPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), aprilTagPose.getHeading())
                 .build();
+        driveAprilTagShootPos = follower.pathBuilder()
+                .addPath(new BezierLine(aprilTagPose, shootPose))
+                .setLinearHeadingInterpolation(aprilTagPose.getHeading(), shootPose.getHeading())
+                .build();
+        driveGrabArtifactOne = follower.pathBuilder()
+                .addPath(new BezierLine(shootPose, turnToGrabOne))
+                .setLinearHeadingInterpolation(shootPose.getHeading(), turnToGrabOne.getHeading())
+                .addPath(new BezierLine(turnToGrabOne, grabOneArtifact))
+                .build(); //test, idk if the two paths will work or if I need to add another setlinerHeadingInterpolaiton thingy or create two seperate paths
     }
 
     public void statePathUpdate() {
         switch(pathState){
             case DRIVE_STARTPOS_SHOOT_POS:
                 follower.followPath(driveStartPosShootPos, true);
-                setPathState(PathState.SHOOT_PRELOAD); //reset timer and make new state
+                setPathState(PathState.LOOK_AT_APRILTAG); //reset timer and make new state
                 break;
-            case SHOOT_PRELOAD:
+            case LOOK_AT_APRILTAG:
                 //check is follower done it's path?
-                if (!follower.isBusy()) {
-                    //TODO add logic to flywheel shooter
+                //check that 3 seconds has elapsed
+                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() > 3) {
                     telemetry.addLine("Done Path 1");
+                    follower.followPath(driveAprilTagShootPos, true);
+                    setPathState(PathState.SHOOT_PRELOAD);
                     //transition to next state
                 }
                 break;
+            case SHOOT_PRELOAD:
+                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() > 1) {
+                    telemetry.addLine("done with Pose 2");
+                    follower.followPath(driveGrabArtifactOne, true);
+                    setPathState(PathState.GRAB_MORE_ARTIFACTS_ONE);
+                    //transition to next state
+                }
+                break;
+            case GRAB_MORE_ARTIFACTS_ONE:
+                if(!follower.isBusy()){
+                    telemetry.addLine("done with Pose 3");
+                }
             default:
                 telemetry.addLine("No state commanded");
                 break;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -75,7 +75,7 @@ public class Shooter implements Runnable{
         this.leftKicker = hardwareMap.get(Servo.class, "lKick");
         this.midKicker = hardwareMap.get(Servo.class, "mKick");
         this.rightKicker = hardwareMap.get(Servo.class,"rKick");
-        this.rightDam = hardwareMap.get(Servo.class, "rightDam");
+//        this.rightDam = hardwareMap.get(Servo.class, "rightDam");
         this.loadedColors = colorSensors.readAllColors();
 
         this.shooterLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -301,13 +301,13 @@ public class Shooter implements Runnable{
     }
 
     public void setDamUp() {
-        rightDam.setPosition(R_DAM_UP_POS);
+//        rightDam.setPosition(R_DAM_UP_POS);
         isDamUp = true;
     }
 
     public void setDamDown() {
 
-        rightDam.setPosition(R_DAM_DOWN_POS);
+//        rightDam.setPosition(R_DAM_DOWN_POS);
         isDamUp = false;
     }
 
